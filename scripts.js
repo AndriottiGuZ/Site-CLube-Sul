4241293a6a2e98d9d730382f4268d5a9da5a1f49
AndriottiGuZ <gugust19@outlook.com>
2026-03-20 19:59:01 -0300

    primeira push do site

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const antbutton = document.getElementById('ant');
+const nextbutton = document.getElementById('next');
+const itens = document.querySelectorAll('.item');
+const pontos = document.querySelectorAll('.ponto');
+const numeroind = document.querySelector('.numeros');
+const list = document.querySelector('.list');
+
+
+let active = 0;
+const total = itens.length;
+let timer; 
+
+function update(direction) {
+        document.querySelector('.item.ativo').classList.remove('ativo');
+        document.querySelector('.ponto.ativo').classList.remove('ativo');
+
+        if (direction > 0) {
+                if (active < 3) { active = active + 1} else { active = 0 }
+
+        } else { if (active > 0) { active = active - 1} else { active = 3 } }
+
+        numeroind.innerHTML = `0${active + 1}`;
+        itens[active].classList.add('ativo');
+        pontos[active].classList.add('ativo');
+    }
+
+
+antbutton.addEventListener('click', function() {
+    update(-1);
+})
+
+
+nextbutton.addEventListener('click', function() {
+    update(1);
+})
